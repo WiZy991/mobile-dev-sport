eedce6a3687aa2040cdb7d1db9ca9bc4bada541f
Stepan <stavchig@mail.ru>
2026-04-10 15:06:44 +1000

    Initial Commit

---
+++ WorldFintess/WorldFintessApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldFintessApp.swift
+//  WorldFintess
+//
+//  Created by Степан on 10.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct WorldFintessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
